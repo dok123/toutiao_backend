cd8c78e705781323c55391234958ea7cb426afde
doki <he3369903209@163.com>
2026-03-29 22:59:20 +0800

    获取新闻分类，解决跨域问题，获取新闻列表、新闻详情，增加浏览量，添加相关推荐

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from routers import news
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# 允许的来源（可以是域名列表）
+origins = [
+    "http://localhost",
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许的源，开发阶段允许所有源，生产阶段根据实际需求配置
+    allow_credentials=True,  # 允许携带cookie
+    allow_methods=["*"],  # 允许的请求方法
+    allow_headers=["*"],  # 允许的请求头
+)
 
 
 @app.get("/")
@@ -11,3 +26,4 @@ async def root():
 
 # 挂载路由（注册路由）
 app.include_router(news.router)
+
--- routers/news.py
+++ routers/news.py
@@ -2,7 +2,7 @@
 # @Author : HaoJun Chen
 # @APP : PyCharm
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from crud import news
@@ -13,12 +13,68 @@ from config.db_conf import get_db
 # tags  分组（标签）
 router = APIRouter(prefix="/api/news", tags=["news"])
 
+
 @router.get("/categories")
 async def get_categories(skip: int = 0, limit: int = 100, db: AsyncSession = Depends(get_db)):
     # 先获取数据库里面的新闻分类数据 -> 先定义模型 -> 封装查询数据的方法
     categories = await news.get_categories(db, skip, limit)
+    print("categories:", categories)
     return {
-        "status": 200,
+        "code": 200,
         "message": "获取新闻分类成功",
         "data": categories
+    }
+
+
+@router.get("/list")
+async def get_news_list(
+        category_id: int = Query(..., alias="categoryId"),  # alias="categoryId" 设置别名
+        page: int = 1,
+        page_size: int = Query(10, alias="pageSize", le=100),
+        db: AsyncSession = Depends(get_db)
+):
+    # 思路：处理分页逻辑 -》 查询新闻列表 -》计算总量 -》 计算是否还有更多
+    offset = (page - 1) * page_size
+    news_list = await news.get_news_list(db, category_id, offset, page_size)
+    total = await news.get_news_count(db, category_id)
+    hsa_more = offset + page_size < total
+    return {
+        "code": 200,
+        "message": "获取新闻列表成功",
+        "data": {
+            "list": news_list,
+            "total": total,
+            "hasMore": hsa_more
+        }
+    }
+
+
+@router.get("/detail")
+async def get_news_detail(news_id: int = Query(..., alias="id"), db: AsyncSession = Depends(get_db)):
+    # 获取新闻详情 + 浏览量+1 + 相关新闻
+    news_detail = await news.get_news_detail(db, news_id)
+    if not news_detail:
+        raise HTTPException(status_code=404, detail="新闻不存在")
+
+    views_result = await news.increase_news_views(db, news_detail.id)
+    if not views_result:
+        raise HTTPException(status_code=500, detail="新闻不存在")
+
+    related_news = await news.get_related_news(db, news_detail.id, news_detail.category_id)
+    print("related_news", related_news)
+
+    return {
+        "code": 200,
+        "message": "success",
+        "data": {
+            "id": news_detail.id,
+            "title": news_detail.title,
+            "content": news_detail.content,
+            "image": news_detail.image,
+            "author": news_detail.author,
+            "publishTime": news_detail.publish_time,
+            "categoryId": news_detail.category_id,
+            "views": news_detail.views,
+            "relatedNews": related_news
+        }
     }
